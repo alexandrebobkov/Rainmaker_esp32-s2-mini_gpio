013d5834a439a56b76f2ddf21d16209c7db8c566
Alexandre B <alexandre.bobkov@gmail.com>
2024-01-03 02:18:51 -0500

    Update app_main.c

--- gpio/main/app_main.c
+++ gpio/main/app_main.c
@@ -1,10 +1,8 @@
-/* GPIO Example
+/* ESP Rainmaker Node
+
+   GPIO Example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
 
    View more examples View -> Command Palette -> ESP Project Examples -> Rainaker
    SDL -> Green -> GPIO35
